ff786b98b292b7e7a593b2c089c728c18cdbe0de
SherylHohman <SherylHohman@users.noreply.github.com>
2016-10-05 00:12:41 -0700

    devops: add express middleware
      npm install:
        body-parser: to parse incomming requests into json
        morgan: to log incoming requests to console for debugging

--- server/index.js
+++ server/index.js
@@ -1,9 +1,20 @@
 // main starting point of application (back end)
 const express = require('express');
 const http = require('http');       // from node
+const bodyParser = require('body-parser');
+const morgan = require('morgan');  // server logging framework
 
 // App Setup
 const app = express();
+// middleware for express: 
+// all requests first go through morgan and bodyParser. 
+// register them with express via app.use (app === express instance)
+
+// logs all incomimg requests to console, useful for debugging
+app.use(morgan('combined'));
+
+// parses all incoming requests into json (tries to anyway)
+app.use(bodyParser.json({ type: '*/*' }));
 
 // Server  Setup
 const port = process.env.PORT || 3000;

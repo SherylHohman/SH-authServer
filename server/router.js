56be0ed74e36edbebe5d1f4df9d7bd2631fe4e7e
SherylHohman <SherylHohman@users.noreply.github.com>
2016-10-23 16:52:13 -0700

    change test data response

--- server/router.js
+++ server/router.js
@@ -1,6 +1,6 @@
 module.exports = function(app) {
   // incoming data, our response, next: for error handling
   app.get('/', function(req, res, next){
-    res.send(['a', 'b', 'c']);
+    res.send(['test', 'data', 'returned from auth server']);
   });
 }

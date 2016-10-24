332f65a5bd54dd518250d91f5210912bb3120629
SherylHohman <SherylHohman@users.noreply.github.com>
2016-10-23 22:40:39 -0700

    main: define a user model schema and class using mongoose

---
+++ server/models/user.js
@@ -0,0 +1,20 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+// Define the model / schema
+const userSchema = new Schema({
+  email: {
+    // JS type of string
+    type: String,
+    // each email must be unique (case insensitive)
+    unique: true,
+    // force data into lowercase, as mongodb isn't case sensitive
+    lowercase: true
+  },
+  password: String
+});
+
+// name our schema 'user', create a model class from it
+const Modelclass = mongoose.model('user', userSchema);
+
+module.exports = ModelClass;

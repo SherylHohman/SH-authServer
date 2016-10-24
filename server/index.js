6fa17f44584435f5cd669164bbc8da5ce5c80843
SherylHohman <SherylHohman@users.noreply.github.com>
2016-10-23 21:40:20 -0700

    edited comments in index.js explaining server setup

--- server/index.js
+++ server/index.js
@@ -9,9 +9,10 @@ const app = express();
 const router = require('./router');
 
 // middleware for express:
-  // all requests first go through morgan and bodyParser.
-  // register middleware with express via app.use
+  // register middleware with express via "app.use"
+  // pass the middleware as a param on the "express.use" function.
   // (app === our express server instance)
+  // we'll send all requests through: morgan and bodyParser, before sending them to our app
 
   // logs all incomimg requests to console, useful for debugging
 app.use(morgan('combined'));
@@ -19,13 +20,23 @@ app.use(morgan('combined'));
   // parses all incoming requests into json (tries to anyway)
 app.use(bodyParser.json({ type: '*/*' }));
 
-// call our routing file with our express server
-// this function defines, intercepts, and "routes" get/put requests
-// to the functions we define for each get/put/.. "route" we define.
+// routing for express:
+  // call our router function (router.js)
+  // passing our express server (app) to it
+  // router function defines, intercepts, and "routes" get/put requests
+  // to the functions we define
+  // for each get/put/.. "route" we define.
 router(app);
 
-// Server  Setup
+
+// Server  Setup:
 const port = process.env.PORT || 3090;
+
+// create an instance of an http server
+  // instance of express named as "app"
+// http is a native node library for handling incoming http requests
+  // forwards (server) requests, to our (express)application
 const server = http.createServer(app);
+
 server.listen(port);
 console.log('Express Server listening on: ' + port);

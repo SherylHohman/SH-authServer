23b0940b39542ee9b2a58cab30fbf83aef0c9564
SherylHohman <SherylHohman@users.noreply.github.com>
2016-10-25 05:02:31 -0700

    fix: signup: empty fields not allowed in db
    - if email or password is empty, don't create new user
    - also handle case of db error on findOne db search

--- server/controllers/authentication.js
+++ server/controllers/authentication.js
@@ -1,4 +1,6 @@
 const User = require('../models/user');
+// html status code for Unprocessable Entity: recognized and syntatically correct request, by semantically erroneous
+const UNPROCESSED = 422;   
 
 exports.signup = function(req, res, next){
   // req: incoming data (at req.body),
@@ -9,8 +11,15 @@ exports.signup = function(req, res, next){
   const email    = req.body.email;
   const password = req.body.password;
 
+  // don't allow empty email or password fields into db
+  // validation for proper email/pass will be handled on front end
+  if (!password || !email) {
+    return res.status(422).send({error: "You must provide email and password"});
+  }
+
   // search User database for given email
   User.findOne({email: email}, function(err, existingUser){
+    if (err) {return next(err);}
 
     if (existingUser) {
       // 422 is http status code for "Bad data.."

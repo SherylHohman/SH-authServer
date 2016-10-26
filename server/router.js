3c3954382777f22d01116ab75e1562bd5590a059
SherylHohman <SherylHohman@users.noreply.github.com>
2016-10-26 01:16:55 -0700

    syn: edit explanatory comments

--- server/router.js
+++ server/router.js
@@ -3,13 +3,18 @@ const passportService = require('./services/passport');
 const passport = require('passport');
 
 // use jwt strategy, and
-// if authenticated, dON'T create a COOKIE Session (passport's default)
+// when authenticated, dON'T create a COOKIE Session (passport's default). we're using tokens
 const requireAuth  = passport.authenticate('jwt',   { session: false });
+
+// local strategy grants auth from email/password.
+// However this step doesn't generate a token,
+// tokens are sent with subsequent requests, and are needed to visit protected routes
 const requireLogin = passport.authenticate('local', { session: false });
 
 module.exports = function(app) {
 
   // test passport (requireAuth defined above) on root route (via postman)
+  // verifies jwt token is valid before allowing access to protected content
   app.get('/', requireAuth, function(req, res) {
     // dummy protected content, that requires auth
     res.send({ dummy: 'protected content' });
@@ -17,6 +22,8 @@ module.exports = function(app) {
 
   // requireLogin from passport.js, THEN Authentication, IF auth'd..
   // first confirm email/password (requireLogin), then get token from .login (2 middleares)
+  // two steps. token is always verified before can reach any 'requireAuth' path
+  // this generates token, if email/password are valid
   app.post('/login', requireLogin, Authentication.login)
 
   // /signup route calls the signup function in our newly created authentication controller

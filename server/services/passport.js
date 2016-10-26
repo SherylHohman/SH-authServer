899d1483da32e7630dc51b216a506c4928e9576c
SherylHohman <SherylHohman@users.noreply.github.com>
2016-10-25 18:44:33 -0700

    main: install passport; define a login jwtStrategy

---
+++ server/services/passport.js
@@ -0,0 +1,59 @@
+const passport = require('passport');
+const JwtStragety = require('passport-jwt').Strategy;
+const ExtractJwt = require('passport-jwt').Extract;
+const config = require('../config');
+const User = require('../models/user');
+
+// a Stragety is a method Passport can use for authenticating a user
+// Passport is a library for handling authentications. Usually is is used for cookie auth, however with passport-jwt, it can also be used with jwt.
+
+// We'll have two passport Strategies:
+// Strategy #1: Verify via JWT: new user signup, or already logged in
+// Stragety #2: Verify via username and password: existing user is logging in
+// could also do github, facebook, etc. goto passport library for more info
+
+//Setup options for this JWT Strategy
+const jwtOptions = {
+  const jwtOptions = {
+    // tell passport where to find our token,
+    jwtFromRequest: ExtractJwt.fromHeader('authorization'),
+
+    // and the key so it can be decoded
+    secretOrkey: config.secret
+  };
+};
+
+// Creat JWT Strategy
+// payload is the DEcoded jwt token
+// jwt will have the "sub" and "iat" properties
+// we need the jwtsecret to decode the jwt token
+
+// done is a passport method, thats called at the end of our "strategy"
+//    and handles granting/denying athentication
+
+const jwtLogin = new JwtStragety(jwtOptions, function(payload, done) {
+
+  // See if the user ID in the payload exists in our db
+  User.findById(payload.sub, function(err, user) {
+    // error connecting to the db, or the search process fails, userobject: false
+    if (err) { return done(err, false); }
+
+    // if found user, return 'null' error, and found user
+    // If it does, call 'done' with that user
+    if (user) {
+      done(null, user);
+
+      // Otherwise, call 'done' without a user object ('false') - don't validate them
+      // user not found, return 'null' no error, 'false' user not found
+    } else {
+      done(null, false);
+    }
+  });
+
+});
+
+
+// Tell Passport to use this Strategy defined (above)
+passport.use(jwtLogin);
+
+
